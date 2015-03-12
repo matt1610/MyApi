148c3b53ea791b7928876cb820efe9a5af62c7cf
matt1610 <mattstarkey@me.com>
2015-03-12 09:07:18 +0200

    New Stats API
    Returns some random stats to test Charts on front end.

--- index.js
+++ index.js
@@ -12,6 +12,87 @@ app.get('/api', function(request, response) {
   	response.send('Working!');
 });
 
+app.get('/api/stats', function (request, response) {
+	setInterval(function () {
+  		response.send(DummyData());
+  	}, 5000);
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+
+
+
+function DummyData () {
+	var randomIndex = RandomTwo(0, People.length);
+	return {
+		Name : People[randomIndex].Name,
+		Surname : People[randomIndex].Surname,
+		HeartRate : RandomTwo(50,170),
+		Temperature : RandomTwo(33,37),
+		Acceleration : RandomTwo(0,5),
+		Loc : People[randomIndex].Loc
+	}
+}
+
+
+
+function RandomTwo (one, two) {
+	return Math.floor(Math.random() * two) + one;
+}
+
+
+var People = [
+	{
+		Name : 'Matthew',
+		Surname : 'Starkey',
+		Age : 30,
+		Weight : 75,
+		Loc : {
+			Lat : -29.723173,
+			Long : 31.063996
+		}
+	},
+	{
+		Name : 'JP',
+		Surname : 'Ras',
+		Age : 29,
+		Weight : 85,
+		Loc : {
+			Lat : -29.716110,
+			Long : 31.066056
+		}
+	},
+	{
+		Name : 'Tim',
+		Surname : 'Baker',
+		Age : 27,
+		Weight : 85,
+		Loc : {
+			Lat : -29.720042,
+			Long : 31.079059
+		}
+	},
+	{
+		Name : 'Grant',
+		Surname : 'Winlow',
+		Age : 30,
+		Weight : 75,
+		Loc : {
+			Lat : -29.715253,
+			Long : 31.070133
+		}
+	},
+	{
+		Name : 'Gary',
+		Surname : 'Nixon',
+		Age : 28,
+		Weight : 83,
+		Loc : {
+			Lat : -29.715253,
+			Long : 31.070133
+		}
+	}
+]
